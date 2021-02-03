140e8cb88133ab2fd4ec63e36edb0c6390f2f354
Jackson Secrist <js311@byu.edu>
2021-02-03 15:24:31 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using FilmCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddFilm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddFilm(Film newFilm)
+        {
+            if (ModelState.IsValid)
+            {
+                if(newFilm.Title != "Independence Day")
+                {
+                    TempStorage.AddFilm(newFilm);
+                    return View("Confirmation", newFilm);
+                }
+                else
+                {
+                    return View("NotStored");
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Collection()
+        {
+            return View(TempStorage.FilmCollection);
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Models
+{
+    public static class TempStorage
+    {
+        private static List<Film> filmCollection = new List<Film>();
+
+        public static IEnumerable<Film> FilmCollection => filmCollection;
+
+        public static void AddFilm(Film newFilm)
+        {
+            filmCollection.Add(newFilm);
+        }
+    }
+}
